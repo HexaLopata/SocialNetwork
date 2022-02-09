a90703c5b1a4feeb1a5e623510f6176d55b3d555
Hexalopata <alekseygasyuk50@gmail.com>
2022-02-09 23:49:42 +0300

    Обновлена конфигурация webpack

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,27 +1,32 @@
 const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
     entry: "./src/index.js",
-    mode: "development",
-    devServer: {
-        contentBase: path.join(__dirname, "dist"),
-        compress: true,
-        port: 9000,
-        watchContentBase: true,
-        progress: true
+    output: {
+        path: path.resolve(__dirname, 'static/frontend'),
+        filename: "[hash].[name].js"
     },
-
+    resolve: { extensions: [".js", ".jsx"] },
+    optimization: {
+        splitChunks: {
+            chunks: 'all'
+        }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            publicPath: '/static/frontend/'
+        }),
+        new CleanWebpackPlugin()
+    ],
     module: {
         rules: [
             {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
-                resolve: {
-                    extensions: [".js", ".jsx"]
-                },
-                use: {
-                    loader: "babel-loader"
-                }
+                use: [ "babel-loader" ]
             },
             {
                 test: /\.css$/,
@@ -29,15 +34,9 @@ module.exports = {
                     "style-loader",
                     {
                         loader: "css-loader",
-                        options: {
-                            modules: true
-                        }
+                        options: { modules: true }
                     }
                 ]
-            },
-            {
-                test: /\.(png|svg|jpg|gif)$/,
-                use: ["file-loader"]
             }
         ]
     }
